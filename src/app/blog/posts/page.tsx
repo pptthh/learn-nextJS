2e64fb7d7c2a5451a74d28246e0ba64c6a7f9301
Tóth Péter <pptthh@gmail.com>
2026-01-19 15:59:23 +0100

    check connected to database

--- src/app/blog/posts/page.tsx
+++ src/app/blog/posts/page.tsx
@@ -2,12 +2,13 @@ import { connectToDB } from '@/app/lib/data';
 import { posts } from '@/app/lib/placeholder-data';
 import Post from '@/app/ui/components/posts/Post';
 
-export default function async Page() {
+export const Page = async () => {
   const client = await connectToDB();
   return (
     <>
-      {client && client?.connect && <p className='text-green-500'>Connected to DB</p>}
+      { client && <p className='text-green-500'>Connected to DB</p>}
       <h1>Posts</h1>
       {posts.map((post) => <Post key={post.id} {...post} />)}
-    </>)
-}
+    </>
+  );
+};
