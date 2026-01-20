6ab7a6bec961502c94e8e4c17a4cb2f61ec8a683
Tóth Péter <pptthh@gmail.com>
2026-01-20 14:00:49 +0100

    might revert !!! --------------------------------

--- src/app/blog/posts/page.tsx
+++ src/app/blog/posts/page.tsx
@@ -1,22 +1,17 @@
-import { connectToDB, getPosts } from '@/app/lib/data';
-// import { posts as _posts } from '@/app/lib/placeholder-data';
+import Link from "next/link"
+import { Button } from "@/app/ui/components/button"
+import { posts } from '@/app/lib/placeholder-data';
 import Post from '@/app/ui/components/posts/Post';
+import { connectToDB, getPosts } from '@/app/lib/data';
 
-const Page = async () => {
+export default async function Page() {
   const client = await connectToDB();
-  const posts = await getPosts() as [];
+  const posts = await getPosts();
   return (
     <>
-      {
-      client ?
-       <p className='text-green-500'>Connected to DB</p> :
-       <p className='text-red-500'>Failed to Connect to DB</p>
-      }
+      {client && <p className='text-green-500 my-2'>Connected to database!</p>}
+      <Link href="/blog/post/insert"><Button className="outline outline-1  border-purple-700 text-purple-700 hover:bg-purple-700 hover:text-white my-5 py-2 px-4 rounded">New +</Button></Link>
       <h1>Posts</h1>
-      {posts?.map((post) => <Post {...post} />)}
-      {/* {_posts?.map((post) => <Post key={post?.id} {...post} />)} */}
-    </>
-  );
-};
-
-export default Page;
+      {posts?.map((post) => <Post key={post.id} {...post} />)}
+    </>)
+}
--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,22 +1,27 @@
-import { createClient, sql, VercelClient } from '@vercel/postgres';
+import { createClient } from '@vercel/postgres';
+import { sql } from '@vercel/postgres';
 
 export async function connectToDB() {
+  const client = createClient();
+  await client.connect();
+
   try {
-    const client = createClient();
-    await client.connect();
-    // console.debug('\nConnected to database');
-    return client;
-  }
-  catch (error) {
-    console.error('\nDatabase Connection Error:\n', error);
+    if (client) {
+      console.log('Connected to database');
+      return client;
+    }
+  } catch (error) {
+    console.error('Error connecting to database', error);
   }
 }
-//*
-export const getPosts = async () => {
-  const posts = await sql `SELECT * FROM posts;`;
-  // if (!posts)
-  //   throw new Error('Failed to fetch posts.');
-  console.debug('posts.rows: ', posts.rows);
-  return posts.rows;
+
+export async function getPosts() {
+  try {
+    const data = await sql`SELECT * FROM posts`
+    // console.log(data.rows)
+    return data.rows;
+  } catch (error) {
+    console.error('Error getting posts', error);
+  }
 }
-/**/// */
+
