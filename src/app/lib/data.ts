6d9d55dafc8aaff4b54fdd11ab55b1bbc8741b70
Tóth Péter <pptthh@gmail.com>
2026-01-20 09:32:26 +0100

    connection works with params: https://vercel.com/toth-peters-projects-67c94b7f/~/integrations/neon/icfg_nLXU2XvqTaKox5nmF0h6WMcH/resources/storage/store_T1OZigm4bjYayMDz/guides

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -2,19 +2,11 @@ import { createClient } from '@vercel/postgres';
 
 export async function connectToDB() {
     try {
-        const client = createClient({
-            // connectionString: process.env.POSTGRES_URL,
-        });
-
+        const client = createClient();
         await client.connect();
-        console.log(
-            client ?
-            "Connected to DB" :
-            "Connection error"
-        ); 
         return client;
     }
     catch (error) {
-        console.error('Database Connection Error:', error);
+        console.error('\nDatabase Connection Error:\n', error);
     }
 }
