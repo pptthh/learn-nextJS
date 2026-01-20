d1023b278311d96703046804bd6f45b4148daa08
Tóth Péter <pptthh@gmail.com>
2026-01-20 12:55:17 +0100

    fetch data works

--- src/app/blog/posts/page.tsx
+++ src/app/blog/posts/page.tsx
@@ -1,9 +1,10 @@
-import { connectToDB } from '@/app/lib/data';
-import { posts } from '@/app/lib/placeholder-data';
+import { connectToDB, getPosts } from '@/app/lib/data';
+import { posts as _posts } from '@/app/lib/placeholder-data';
 import Post from '@/app/ui/components/posts/Post';
 
 const Page = async () => {
   const client = await connectToDB();
+  const posts = await getPosts();
   return (
     <>
       {
@@ -12,7 +13,9 @@ const Page = async () => {
        <p className='text-red-500'>Failed to Connect to DB</p>
       }
       <h1>Posts</h1>
-      {posts.map((post) => <Post key={post.id} {...post} />)}
+      {/* {posts.map((post) => <Post key={post.id} {...post} />)} */}
+      <hr />
+      {_posts.map((post) => <Post key={post.id} {...post} />)}
     </>
   );
 };
--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,12 +1,22 @@
-import { createClient } from '@vercel/postgres';
+import { createClient, sql, VercelClient } from '@vercel/postgres';
 
 export async function connectToDB() {
-    try {
-        const client = createClient();
-        await client.connect();
-        return client;
-    }
-    catch (error) {
-        console.error('\nDatabase Connection Error:\n', error);
-    }
+  try {
+    const client = createClient();
+    await client.connect();
+    // console.debug('\nConnected to database');
+    return client;
+  }
+  catch (error) {
+    console.error('\nDatabase Connection Error:\n', error);
+  }
 }
+//*
+export const getPosts = async () => {
+  const posts = await sql `SELECT * FROM posts;`;
+  // if (!posts)
+  //   throw new Error('Failed to fetch posts.');
+  console.debug('posts.rows: ', posts.rows);
+  return posts.rows;
+}
+/**/// */
