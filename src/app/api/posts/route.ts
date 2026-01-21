a39dac7cd9738a60373e558be0b751e1602cfb3f
Tóth Péter <pptthh@gmail.com>
2026-01-21 15:14:03 +0100

    testing

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -20,9 +20,12 @@ export async function POST(request: Request) {
 
   try {
     // SQL query to insert a new post
-    await sql`INSERT INTO posts (id, author, title, content, date) VALUES (${id}, 'sandra l', ${title}, ${content}, ${date});`;
-    return NextResponse.json({ message: 'Post successfully inserted' }, { status: 200 });
+    // await sql `INSERT INTO posts (id, title, content, date) VALUES (${id}, ${title}, ${content}, ${date})`;
+    const post = await sql `INSERT INTO posts (id, author, title, content, date) VALUES (${id}, 'sandra l', ${title}, ${content}, ${date});`;
+    console.log('Insert post:', post);
+    return NextResponse.json({ result: post, message: 'Post successfully inserted' }, { status: 200 });
   } catch (error) {
+    console.log('Error inserting post:', error);
     return NextResponse.json({ error }, { status: 500 });
   }
 }
--- src/app/blog/posts/page.tsx
+++ src/app/blog/posts/page.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link"
 import { Button } from "@/app/ui/components/button"
-import { posts } from '@/app/lib/placeholder-data';
 import Post from '@/app/ui/components/posts/Post';
 import { connectToDB, getPosts } from '@/app/lib/data';
 import { iPost } from "@/app/lib/interfaces";
@@ -10,7 +9,7 @@ export default async function Page() {
   const posts = await getPosts() as iPost[] | undefined;
   return (
     <>
-      {client && <p className='text-green-500 my-2'>Connected to database!</p>}
+      {client && posts && <p className='text-green-500 my-2'>Connected to database! (Length:{posts.length})</p>}
       <Link href="/blog/post/insert"><Button className="outline outline-1  border-purple-700 text-purple-700 hover:bg-purple-700 hover:text-white my-5 py-2 px-4 rounded">New +</Button></Link>
       <h1>Posts</h1>
       {posts?.map((post) => <Post key={post.id} {...post} />)}
