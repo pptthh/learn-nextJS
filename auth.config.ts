7d11b9e66a15a21dd1227a92c483e3af34bd12dd
Tóth Péter <pptthh@gmail.com>
2026-01-23 14:27:25 +0100

    play with auth ... not working

--- auth.config.ts
+++ auth.config.ts
@@ -1,5 +1,21 @@
 import type { NextAuthConfig } from 'next-auth';
 
 export const authConfig = {
+  pages: {
+    signIn: '/login',
+  },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnPostInsert = nextUrl.pathname.startsWith('/blog/post/insert');
+      console.log('Auth callback - authorized called', { auth, nextUrl, isLoggedIn, isOnPostInsert });
+      if (isOnPostInsert) {
+        return (isLoggedIn); // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL('/', nextUrl));
+      }
+      return true;
+    },
+  },
   providers: [], // Add providers with an empty array for now
 } satisfies NextAuthConfig;
