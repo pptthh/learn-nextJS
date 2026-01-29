db2df0010e2398a21e556c9dfba8f53408c06f29
Tóth Péter <pptthh@gmail.com>
2026-01-29 14:30:58 +0100

    refactoring / updateing something improved with the style

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -2,7 +2,7 @@
 import SideNav from "@/app/ui/components/sidenav";
 import { SessionProvider } from "next-auth/react";
 
-export default async function Layout({ children }: { children: React.ReactNode}) {
+export default function Layout({ children }: { children: React.ReactNode}) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden bg-white">
       <div className="w-full flex-none md:w-64">
