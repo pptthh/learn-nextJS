938cd2ee126a6d536d584e0762e13e1e52340392
Tóth Péter <pptthh@gmail.com>
2026-01-29 14:10:05 +0100

    refactoring / updateing

--- src/app/ui/components/sidenav.tsx
+++ src/app/ui/components/sidenav.tsx
@@ -7,8 +7,8 @@ export default function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <div className="flex grow flex-row space-x-2 md:flex-col md:space-x-0 md:space-y-2">
-        <NavLinks />
         <SessionProvider>
+          <NavLinks />
           <UserButton />
         </SessionProvider>
       </div>
